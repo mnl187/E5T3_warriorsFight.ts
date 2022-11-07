ec4dae84673e3c65aad92bbd522cb46160af79fb
mnl187 <mnl187@gmail.com>
2022-11-07 07:40:57 +0100

    przepisanie js na ts, wyodrębienienie classy Warrior

--- index.ts
+++ index.ts
@@ -1,32 +1,3 @@
-class Warrior {
-    constructor(name, hitPoints, hp) {
-        this.name = name;
-        this.hitPoints = hitPoints;
-        this.hp = hp;
-    }
-
-    setHp(hp) {
-        this.hp = hp;
-    }
-
-    getHp() {
-        return this.hp;
-    }
-
-    getHitPoints() {
-        return this.hitPoints;
-    }
-
-    getName() {
-        return this.name;
-    }
-
-    levelUp() {
-        this.hitPoints += 1.1;
-        this.hp += 1.1;
-    }
-}
-
 class Arena {
     constructor(warrior1, warrior2) {
         if (!(warrior1 instanceof Warrior)) {
