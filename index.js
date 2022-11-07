0e21742791a36f9049e434bcf8ca9180fd69cd7b
mnl187 <mnl187@gmail.com>
2022-11-07 06:41:10 +0100

    dodanie classy Arena

--- index.js
+++ index.js
@@ -24,7 +24,16 @@ class Warrior {
     levelUp() {
         this.hitPoints += 1.1;
         this.hp += 1.1;
+    }
+}
 
-
+class Arena {
+    constructor(warrior1, warrior2) {
+        if (!(warrior1 instanceof Warrior)) {
+            throw new Error('Warrior1 must be an instance of Warrior class!');
+        }
+        if (!(warrior2 instanceof Warrior)) {
+            throw new Error('Warrior2 must be an instance of Warrior class!');
+        }
     }
 }
